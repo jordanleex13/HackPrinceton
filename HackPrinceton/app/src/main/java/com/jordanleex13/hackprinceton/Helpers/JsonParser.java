1c9fba3f4f02e12c0e1fa0a31695ae76a201bf96
Jason Piao <jason.piao@mail.utoronto.ca>
2016-11-12 01:50:31 -0500

    Json

--- HackPrinceton/app/src/main/java/com/jordanleex13/hackprinceton/Helpers/JsonParser.java
+++ HackPrinceton/app/src/main/java/com/jordanleex13/hackprinceton/Helpers/JsonParser.java
@@ -1,7 +1,17 @@
 package com.jordanleex13.hackprinceton.Helpers;
 
-import org.json.JSONException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
@@ -10,61 +20,53 @@ import java.io.IOException;
 
 public class JsonParser {
 
-    String base = "https://api.seatgeek.com/2";
+    String base = "https://api.seatgeek.com/2/events/3315199";
 
     public void httpGet(String searchTerm) throws IOException, JSONException {
 
-//        JobManager.setSearchTerm(searchTerm);
-//
-//        HttpClient httpclient = new DefaultHttpClient();
-//        HttpResponse response = httpclient.execute(new HttpGet(in));
-//
-//
-//        StatusLine statusLine = response.getStatusLine();
-//
-//        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-//            ByteArrayOutputStream out = new ByteArrayOutputStream();
-//            response.getEntity().writeTo(out);
-//
-//
-//            String responseString = out.toString();
-//            Log.d(TAG, responseString);
-//
-//
-//
-//            JSONObject reader = new JSONObject(responseString);
-//
-//            //Get the instance of JSONArray that contains JSONObjects
-//            JSONArray jsonArray = reader.optJSONArray("results");
-//            Log.e(TAG, "Num of jobs is " + String.valueOf(jsonArray.length()));
-//            for(int i=0; i < jsonArray.length(); i++){
-//                JSONObject jsonObject = jsonArray.getJSONObject(i);
-//
-//                String jobTitle = jsonObject.optString("jobtitle");
-//                String company = jsonObject.optString("company");
-//                String city = jsonObject.optString("city");
-//                String state = jsonObject.optString("state");
-//                String country = jsonObject.optString("country");
-//                String description = jsonObject.optString("snippet");
-//                String url = jsonObject.optString("url");
-//                float latitude = Float.parseFloat(jsonObject.optString("latitude"));
-//                float longitude = Float.parseFloat(jsonObject.optString("longitude"));
-//
-//
-//                Job curr = new Job(jobTitle, company, city, state, country, description, url, latitude, longitude);
-//                JobManager.addJob(curr);
-//
-//                publishProgress();
-//
-//            }
-//
-//
-//
-//            out.close();
-//        } else{
-//            //Closes the connection.
-//            response.getEntity().getContent().close();
-//
-//        }
+       String in = "https://api.seatgeek.com/2/events/3315199";
+
+       HttpClient httpclient = new DefaultHttpClient();
+       HttpResponse response = httpclient.execute(new HttpGet(in));
+
+
+       StatusLine statusLine = response.getStatusLine();
+
+       if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+           ByteArrayOutputStream out = new ByteArrayOutputStream();
+           response.getEntity().writeTo(out);
+
+
+           String responseString = out.toString();
+           //Log.d(TAG, responseString);
+
+
+
+           JSONObject reader = new JSONObject(responseString);
+
+           //Get the instance of JSONArray that contains JSONObjects
+           JSONArray jsonArray = reader.optJSONArray("results");
+           //Log.e(TAG, "Num of jobs is " + String.valueOf(jsonArray.length()));
+           for(int i=0; i < jsonArray.length(); i++){
+               JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+               String id = jsonObject.optString("id");
+               String title = jsonObject.optString("title");
+               String date = jsonObject.optString("datetime_local");
+               String ticket_url = jsonObject.optString("url");
+               String city = jsonObject.optString("city");
+               String[] location = jsonObject.optString("location");
+               System.out.println(location[0]);
+
+           }
+
+
+
+           out.close();
+       } else{
+           //Closes the connection.
+           response.getEntity().getContent().close();
+
+       }
     }
 }
